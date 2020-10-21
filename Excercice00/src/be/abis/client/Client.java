a62ab36c3f6c7203f5bad37e3d77e83d01a254ad
Xavier Aupaix <Xavier.AUPAIX@sfpd.fgov.be>
2020-10-21 13:45:33 +0200

    ajout du main dans la classe Client

--- Excercice00/src/be/abis/client/Client.java
+++ Excercice00/src/be/abis/client/Client.java
@@ -6,6 +6,13 @@ public class Client {
 
 	private Shape myShape;
 
+	public static void main (String[] args){
+		Client client =new Client(new Rectangle(10.0,10.0,"red"));
+		System.out.println(client.getMyShape().getColor());
+		System.out.println(client.getMyShape().area());
+
+	}
+
 	public Client(Rectangle rectangle){
 		this.myShape=rectangle;
 	}
